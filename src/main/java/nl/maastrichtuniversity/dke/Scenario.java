f6751b133a5b8033ab1a2c3e3572b5dc899025d6
DaanSchar <Daan.Schaer@Gmail.com>
2022-02-03 18:15:43 +0100

    merged environment and scenario

--- src/main/java/nl/maastrichtuniversity/dke/Scenario.java
+++ src/main/java/nl/maastrichtuniversity/dke/Scenario.java
@@ -11,16 +11,38 @@ public class Scenario {
     private double timeStep;
 
     private List<Agent> agents;
+    private Environment environment;
 
-    // private Environment environment;
-
-    public Scenario(String name, int gameMode, double scaling, double timeStep, List<Agent> agents) {
+    public Scenario(String name, int gameMode, double scaling, double timeStep, List<Agent> agents, Environment environment) {
         this.name = name;
         this.agents = agents;
+        this.gameMode = gameMode;
+        this.scaling = scaling;
+        this.timeStep = timeStep;
+        this.environment = environment;
     }
 
+    public String getName() {
+        return name;
+    }
 
+    public double getScaling() {
+        return scaling;
+    }
 
+    public double getTimeStep() {
+        return timeStep;
+    }
 
+    public Environment getEnvironment() {
+        return environment;
+    }
 
+    public int getGameMode() {
+        return gameMode;
+    }
+
+    public List<Agent> getAgents() {
+        return agents;
+    }
 }
