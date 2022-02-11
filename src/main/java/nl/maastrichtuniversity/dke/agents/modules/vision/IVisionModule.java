73f76b518c69e2f44cab49a4eec19c07d7ba3a40
DaanSchar <Daan.Schaer@Gmail.com>
2022-02-11 22:03:28 +0100

    changed it that you don't need the agent

--- src/main/java/nl/maastrichtuniversity/dke/agents/modules/vision/IVisionModule.java
+++ src/main/java/nl/maastrichtuniversity/dke/agents/modules/vision/IVisionModule.java
@@ -1,13 +1,12 @@
 package nl.maastrichtuniversity.dke.agents.modules.vision;
 
-import nl.maastrichtuniversity.dke.agents.Agent;
 import nl.maastrichtuniversity.dke.areas.Area;
 import nl.maastrichtuniversity.dke.util.Vector;
 
 import java.util.List;
 
 public interface IVisionModule {
-    List<Area> getObstacles();
+    List<Area> getObstacles(Vector direction);
 
 
 
--- src/main/java/nl/maastrichtuniversity/dke/agents/modules/vision/VisionModule.java
+++ src/main/java/nl/maastrichtuniversity/dke/agents/modules/vision/VisionModule.java
@@ -1,70 +1,64 @@
 package nl.maastrichtuniversity.dke.agents.modules.vision;
 
-import lombok.Getter;
 import nl.maastrichtuniversity.dke.scenario.Environment;
 import nl.maastrichtuniversity.dke.util.Vector;
-import nl.maastrichtuniversity.dke.agents.Agent;
 import nl.maastrichtuniversity.dke.areas.Area;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class VisionModule implements IVisionModule{
-    List<Ray> Rays;
+
+    private Environment environment;
+
     private final int FOV_RESOLUTION = 50;
     private final int RAY_RESOLUTION = 50;
     private final double VISION_LENGTH = 10;
-    Agent a;
-    Environment environment;
-    private  List<Area> seenObjects;
+    private final double fov;
 
-    public VisionModule(Agent a, Environment environment){
-        this.a=a;
+    private List<Ray> rays;
+
+    public VisionModule(Environment environment, double fov){
         this.environment = environment;
-        seenObjects = new ArrayList<>();
+        this.fov = fov;
     }
 
-    private void calculateRays(){
-        double fov = a.getFov();
-        double rays = FOV_RESOLUTION;
-        double interval = fov/rays;
+    private void calculateRays(Vector direction){
+        double totalRays = FOV_RESOLUTION;
+        double interval = fov/totalRays;
+        this.rays = new ArrayList<>();
 
-        Vector direction  = a.getDirection();
         Vector initialRay = direction.rotate(-fov/2).mul(VISION_LENGTH);
 
-        for(int i = 0; i<rays;i++){
-            Rays.add(new Ray(initialRay.rotate(i*interval)));
-        }
-
-        for (Area area : environment.getObjects()) {
-            for (Ray ray : Rays) {
-                if (ray.hasPointInArea(area)) {
-                    seenObjects.add(area);
-                }
-            }
-        }
-
-
+        for(int i = 0; i < totalRays; i++)
+            this.rays.add(new Ray(initialRay.rotate(i*interval)));
     }
 
     @Override
-    public List<Area> getObstacles() {
-        return seenObjects;
+    public List<Area> getObstacles(Vector direction) {
+        calculateRays(direction);
+
+        List<Area> obstacles = new ArrayList<>();
+
+        for (Area area : environment.getObjects()) {
+            for (Ray ray : rays) {
+                if (ray.hasPointInArea(area)) obstacles.add(area);
+            }
+        }
+
+        return obstacles;
     }
 
 
     private class Ray {
 
-        private final Vector ray;
         private List<Vector> points;
 
         public Ray(Vector ray) {
-            this.ray = ray;
             points = new ArrayList<>();
             for (int i = 1; i <= RAY_RESOLUTION; i++) {
                 points.add(ray.mul(i / (double) RAY_RESOLUTION));
             }
-
         }
 
         public boolean hasPointInArea(Area area) {
@@ -91,8 +85,6 @@ public class VisionModule implements IVisionModule{
         public boolean pointIsInArea(Vector point, Area area) {
             return false;
         }
-
-
 
     }
 
