584856d50a6cbe84c29fd178eaf2aa3274f06dda
DaanSchar <Daan.Schaer@Gmail.com>
2022-02-11 21:47:59 +0100

    rewrote collision system

--- src/main/java/nl/maastrichtuniversity/dke/areas/Collider.java
+++ src/main/java/nl/maastrichtuniversity/dke/areas/Collider.java
@@ -1,19 +1,86 @@
 package nl.maastrichtuniversity.dke.areas;
 
+import nl.maastrichtuniversity.dke.util.DebugSettings;
 import nl.maastrichtuniversity.dke.util.Vector;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.logging.Logger;
+
 
 /**
- * GJK algorithm for collision detection between 2 polynomials.
- * source: https://www.youtube.com/watch?v=ajv46BSqcK4
+ * Collision handling class for rectangles and circles.
+ *
+ *
+ * source for circle-rectangle : https://stackoverflow.com/questions/401847/circle-rectangle-collision-detection-intersection
  *
  * @Author Daan
  */
 
 public class Collider {
 
+    private static Logger logger = Logger.getLogger(Collider.class.getName());
+
+    public static boolean collides(Area area1, Area area2) {
+        if (isColliding(area1, area2)) {
+            if (DebugSettings.DEBUG_COLLISION)
+                logger.info("Collision detected between shape " +
+                        "[id=" + area1.getId() + " (" + area1.getClass().getSimpleName() + ")] " + " and shape " +
+                        "[id=" + area2.getId() + " (" + area2.getClass().getSimpleName() + ")]"
+                );
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean isColliding(Area s1, Area s2) {
+
+        if (s1 instanceof Rectangle)
+            if (s2 instanceof Rectangle)
+                return collides((Rectangle) s1, (Rectangle) s2);
+            else if (s2 instanceof Circle)
+                return collides((Rectangle) s1, (Circle) s2);
+
+        if (s1 instanceof Circle)
+            if (s2 instanceof Rectangle)
+                return collides((Rectangle) s2, (Circle) s1);
+            else if (s2 instanceof Circle)
+                return collides((Circle) s1, (Circle) s2);
+
+        return false;
+    }
+
+
+    // rectangle-rectangle collision detection
+    private static boolean collides(Rectangle r1, Rectangle r2) {
+       return (r1.getPosition().getX() < r2.getPosition().getX() + r2.getWidth() &&
+               r1.getPosition().getX() + r1.getWidth() > r2.getPosition().getX()
+       ) && (
+               r1.getPosition().getY() < r2.getPosition().getY() + r2.getHeight() &&
+               r1.getPosition().getY() + r1.getHeight() > r2.getPosition().getY()
+       );
+    }
+
+    // circle-circle collision detection
+    private static boolean collides(Circle c1, Circle c2) {
+        logger.info(c1.getPosition().getDistance(c2.getPosition()) + " < " + (c1.getRadius() + c2.getRadius()));
+        return c1.getPosition().getDistance(c2.getPosition()) < c1.getRadius() + c2.getRadius();
+    }
+
+    // rectangle-circle collision detection
+    private static boolean collides(Rectangle rec, Circle cir) {
+        Vector recCenter = rec.getPosition().add(new Vector(rec.getWidth()/2.0 , rec.getHeight()/2.0 ));
+        Vector circleDistance = cir.getPosition().sub(recCenter).abs();
+
+        if (circleDistance.getX() > (rec.getWidth() / 2.0 + cir.getRadius())) return false;
+        if (circleDistance.getY() > (rec.getHeight() / 2.0 + cir.getRadius())) return false;
+
+        if (circleDistance.getX() <= (rec.getWidth() / 2.0)) return true;
+        if (circleDistance.getY() <= (rec.getHeight() / 2.0)) return true;
+
+        double cornerDistance_sq = Math.pow(circleDistance.getX() - (rec.getWidth() / 2.0), 2) +
+                Math.pow(circleDistance.getY() - (rec.getHeight() / 2.0), 2);
+
+        return (cornerDistance_sq <= Math.pow(cir.getRadius(), 2));
+    }
 
 
 
