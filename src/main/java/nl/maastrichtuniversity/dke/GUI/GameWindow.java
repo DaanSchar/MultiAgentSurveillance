11c49f07ef27625e3ea0aa2a1e4fe9eede834d97
DaanSchar <Daan.Schaer@Gmail.com>
2022-02-12 21:34:44 +0100

    a little more cleaning

--- src/main/java/nl/maastrichtuniversity/dke/GUI/GameWindow.java
+++ src/main/java/nl/maastrichtuniversity/dke/GUI/GameWindow.java
@@ -14,27 +14,20 @@ import nl.maastrichtuniversity.dke.scenario.Environment;
 public class GameWindow  {
 		
 
-    /**
-     * Create GameComponent name it game
-     */	
-	private GameComponent game;
 
-    /**
-     * set the icon of the game
-     */
+    // Create GameComponent name it game
+    private GameComponent game;
+
+     // set the icon of the game
     private ImageIcon icon = new ImageIcon(Objects.requireNonNull(GameWindow.class.getResource("/images/settings/icon.png")));
 
-    /**
-     * Creating new Frame (window)
-     */
-
+    // creating new frame (window)
     private JFrame window = new JFrame("GROUP 14");
 
-    /**
+    /*
      * Create variables for the background image
      * the back button 
      */
-
     private ImageIcon backImage = new ImageIcon(Objects.requireNonNull(GameWindow.class.getResource("/images/settings/next.jpg")));
     private JButton back = new JButton("BACK");
 
@@ -66,16 +59,15 @@ public class GameWindow  {
         * Implement the back button
         * set the bounds,the background color,the border and add the actionlistener
         */
-
         back.setBounds(0 ,0,75,40);
         back.addActionListener(animationListener);
         back.setBackground(color1);
         back.setFocusable(false);
         back.setBorder(BorderFactory.createBevelBorder(0, Color.gray , Color.black));
-        /*
-         * Implement the exit button. set the bounds,the background color,the border and add the actionlistener
-         */
 
+        /*
+         * Implement the exit button. set the bounds,the background color,the border and add the action listener
+         */
         exit.setBounds(((int)environment.getWidth()*10)-75,0,75,40);
         exit.addActionListener(animationListener);
         exit.setBackground(color1);
@@ -117,7 +109,7 @@ public class GameWindow  {
          *  Adding the label to the frame.
          *  Set the frame visible.
          *  Add back button to the frame
-         *  Add the gamecomponent to the frame
+         *  Add the gameComponent to the frame
          */
         window.addMouseWheelListener(mouseListener);
         window.addMouseListener(mouseListener);
@@ -135,56 +127,54 @@ public class GameWindow  {
 
 
 
- /**
-   *  Creat an Action listener class.
-   *  e: which is the buttons that have an ActionListener.
-   */
-class AnimationListener implements ActionListener {
+    /**
+     *  Creat an Action listener class.
+     *  e: which is the buttons that have an ActionListener.
+     */
+    class AnimationListener implements ActionListener {
 
+        public void actionPerformed(ActionEvent e){
 
-    public void actionPerformed(ActionEvent e){       
-
-   /*
-    * If back button clicked close the frame and go back to GUIplay class
-    * set the bounds
-    */
-        if(e.getSource()==back){
-            window.dispose();
-            Menu Menu = new Menu();
-        }
-        if(e.getSource()==exit){
-    	    System.exit(0);
-    	    }
-         if(e.getSource()==zoomOut){
-            game.zoomOut();
-           }   
+            /*
+            * If back button clicked close the frame and go back to GUIplay class
+            * set the bounds
+            */
+            if(e.getSource()==back){
+                window.dispose();
+                Menu Menu = new Menu();
+            }
+            if(e.getSource()==exit){
+                System.exit(0);
+            }
+            if(e.getSource()==zoomOut){
+                game.zoomOut();
+            }
             if(e.getSource()==zoomIn){
-           game.zoomIn();
-           }
-           if(e.getSource()==resize){
-           game.resize();
-           }
+               game.zoomIn();
+            }
+            if(e.getSource()==resize){
+               game.resize();
+            }
+            window.repaint();
+        }
 
-                  
-    window.repaint();             
-
-}
     }
- class MouseSpy implements MouseWheelListener ,MouseMotionListener,MouseListener {
-    Point point0;
-       boolean released = false;
 
-         @Override
-            public void mouseWheelMoved(MouseWheelEvent e){
-                if (e.getWheelRotation()<0) {
-                    game.zoomIn(); 
-                }
-                else {
-                    game.zoomOut();
-                }
-             window.repaint();
+    class MouseSpy implements MouseWheelListener, MouseMotionListener, MouseListener {
+        Point point0;
+        boolean released = false;
 
-         }
+        @Override
+        public void mouseWheelMoved(MouseWheelEvent e){
+            if (e.getWheelRotation()<0) {
+                game.zoomIn();
+            }
+            else {
+                game.zoomOut();
+            }
+
+            window.repaint();
+        }
 
         @Override
         public void mouseDragged(MouseEvent e) {
@@ -202,7 +192,7 @@ class AnimationListener implements ActionListener {
 
         @Override
         public void mouseClicked(MouseEvent e) {
-    
+
         }
 
         @Override
@@ -219,15 +209,12 @@ class AnimationListener implements ActionListener {
         @Override
         public void mouseEntered(MouseEvent e) {
             // TODO Auto-generated method stub
-            
         }
 
         @Override
         public void mouseExited(MouseEvent e) {
             // TODO Auto-generated method stub
-            
         }
-         
     }
 }
 
