1c715ca9b53cf26b38d763a095f5ff1566d7fc11
DaanSchar <Daan.Schaer@Gmail.com>
2022-02-15 20:06:19 +0100

    cleaned up vector again

--- src/main/java/nl/maastrichtuniversity/dke/util/Vector.java
+++ src/main/java/nl/maastrichtuniversity/dke/util/Vector.java
@@ -1,5 +1,6 @@
 package nl.maastrichtuniversity.dke.util;
 
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,6 +17,7 @@ import java.util.Objects;
 @ToString
 @Getter
 @Setter
+@EqualsAndHashCode
 public class Vector {
 
     private double x;
@@ -34,6 +36,10 @@ public class Vector {
         this.z = z;
     }
 
+    /**
+     * returns the length of this vector.
+     * @return length of this vector.
+     */
     public double length() {
         return Math.sqrt(
                 Math.pow(this.x, 2) +
@@ -42,8 +48,16 @@ public class Vector {
         );
     }
 
+    /**
+     * returns the unit vector of this vector, which is the vector
+     * with the same direction but a length of 1.
+     * @return vector with length 1.
+     */
     public Vector norm() {
         double length = this.length();
+
+        if (this.isZero())
+            return new Vector(0, 0, 0);
 
         return new Vector(
                 this.x / length,
@@ -52,7 +66,12 @@ public class Vector {
         );
     }
 
-    public double getDistance(Vector other) {
+    /**
+     * computes the distance between two vectors.
+     * @param other the other vector.
+     * @return distance between this and other.
+     */
+    public double distance(Vector other) {
         return Math.sqrt(
                 Math.pow(this.x - other.getX(), 2) +
                 Math.pow(this.y - other.getY(), 2) +
@@ -60,6 +79,11 @@ public class Vector {
         );
     }
 
+    /**
+     * multiplies this vector, or all components of the vector, by a scalar.
+     * @param scalar the scalar we multiply by.
+     * @return multiplied vector.
+     */
     public Vector mul(double scalar) {
         return new Vector(
                 this.x * scalar,
@@ -142,19 +166,6 @@ public class Vector {
         );
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Vector vector = (Vector) o;
-        return Double.compare(vector.x, x) == 0 && Double.compare(vector.y, y) == 0 && Double.compare(vector.z, z) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y, z);
-    }
-
     /**
      * rotates the vector around the z axis (so 2D)
      * @param angle the angle of rotation in degrees
@@ -164,7 +175,23 @@ public class Vector {
         double x1 = (this.x * Math.cos(Math.toRadians(angle)) - this.y * Math.sin(Math.toRadians(angle)));
         double y1 = (this.x * Math.sin(Math.toRadians(angle)) + this.y * Math.cos(Math.toRadians(angle))) ;
         return new Vector(x1, y1);
+    }
 
+    /**
+     * computes the angle between two vectors.
+     * @param other the other vector.
+     * @return angle in degrees
+     */
+    public double angle(Vector other) {
+        return Math.acos(this.dot(other) / (this.length() * other.length())) / Math.PI * 180;
+    }
+
+    /**
+     * computes the angle between the vector and the x-axis
+     * @return angle in degrees
+     */
+    public double angle() {
+        return Math.atan2(this.y, this.x) / Math.PI * 180;
     }
 
     /**
@@ -176,5 +203,13 @@ public class Vector {
                 Math.abs(this.y),
                 Math.abs(this.z)
         );
+    }
+
+    /**
+     * checks if the vector is zero.
+     * @return true if the vector is zero.
+     */
+    public boolean isZero() {
+        return this.x == 0 && this.y == 0 && this.z == 0;
     }
 }
