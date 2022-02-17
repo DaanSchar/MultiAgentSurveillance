397b5129adfc83211a4c45afce01a0a6c0ffcb4b
Parand <p.mohri@student.maastrichtuniversity.nl>
2022-02-17 12:01:38 +0100

    add collision check

--- src/main/java/nl/maastrichtuniversity/dke/agents/modules/movement/Movement.java
+++ src/main/java/nl/maastrichtuniversity/dke/agents/modules/movement/Movement.java
@@ -1,13 +1,10 @@
 package nl.maastrichtuniversity.dke.agents.modules.movement;
 
-import nl.maastrichtuniversity.dke.agents.Agent;
 import nl.maastrichtuniversity.dke.agents.Direction;
 import nl.maastrichtuniversity.dke.agents.modules.AgentModule;
-import nl.maastrichtuniversity.dke.areas.Area;
-import nl.maastrichtuniversity.dke.areas.Circle;
 import nl.maastrichtuniversity.dke.discrete.Scenario;
+import nl.maastrichtuniversity.dke.discrete.TileType;
 import nl.maastrichtuniversity.dke.util.Position;
-import nl.maastrichtuniversity.dke.util.Vector;
 
 /**
  * move the agent to the desirable position
@@ -27,52 +24,44 @@ public class Movement extends AgentModule implements IMovement {
 
     @Override
     public Position goForward(Position position, Direction direction) {
-//        if(checkCollision(position.add(direction)) != null){
-//            return positionCollision(checkCollision(position.add(direction)), position, direction);
-//        }
-        return position.add(new Position(direction.getMoveX(), direction.getMoveY()));
+        Position newPos = position.add(new Position(direction.getMoveX(), direction.getMoveY()));
+        if(checkCollision(newPos)){
+            return position;
+        }
+            return newPos;
     }
 
     @Override
     public Position sprint(Position position, Direction direction) {
-//        if(checkCollision(position.add(direction.mul(2))) != null){
-//            return positionCollision(checkCollision(position.add(direction.mul(2))), position, direction);
-//        }
-        return position.add(new Position(direction.getMoveX() * 2, direction.getMoveY() * 2));
+        Position newPos = position.add(new Position(direction.getMoveX() * 2, direction.getMoveY() * 2));
+        if (checkCollision(newPos)) {
+            return position;
+        }
+            return newPos;
+
     }
 
-    @Override
-    public Position goBackward(Position position, Direction direction) {
-//        if(checkCollision(position.add(direction.mul(-1)))!= null){
-//            return positionCollision(checkCollision(position.add(direction.mul(-1))), position, direction);
-//        }
-        return position.sub(new Position(direction.getMoveX(), direction.getMoveY()));
+        @Override
+        public Position goBackward (Position position, Direction direction) {
+            Position newPos = position.sub(new Position(direction.getMoveX(), direction.getMoveY()));
+            if (checkCollision(newPos)) {
+                return position;
+            }
+            return newPos;
+        }
+
+        /**
+         *  check if there is a collision between agent and an object
+         * @param position the position want to be checked
+         * @return the area with collision or null if there is no collision
+         */
+        private boolean checkCollision(Position position){
+            if (scenario.getEnvironment().getTileMap()[position.getX()][position.getY()].getType() == TileType.EMPTY) {
+                return false;
+            }
+            return true;
+        }
     }
 
-    /**
-     *  check if there is a collision between agent and an object
-     * @param position
-     * @return the area with collision or null if there is no collision
-     */
-    private Area checkCollision(Position position ){
-        Circle agent = new Circle(position.getX(), position.getY(), 0.5);
-//        for(Position area: scenario.getEnvironment().getTileMap()){
-//            if(area.isCollidingWith(agent)){
-//                return area;
-//            }
-//        }
-        return null;
-    }
 
-    /**
-     * calculate the new position after collision
-     * @param area the area agent have collision with
-     * @param position position of the agent
-     * @param direction direction of agent
-     * @return the new position of agent
-     */
-    private Position positionCollision(Area area, Position position, Direction direction ){
-        return position;
-    }
 
-}
