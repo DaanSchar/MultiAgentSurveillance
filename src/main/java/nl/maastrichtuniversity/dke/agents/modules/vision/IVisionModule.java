368e67fecfdf446322fe41846598aa79e3dd1621
danpariiDKE <danpariiuni@gmail.com>
2022-02-19 14:05:07 +0100

    added getObstacles in VisionModule created tests for getObstacles

--- src/main/java/nl/maastrichtuniversity/dke/agents/modules/vision/IVisionModule.java
+++ src/main/java/nl/maastrichtuniversity/dke/agents/modules/vision/IVisionModule.java
@@ -2,11 +2,12 @@ package nl.maastrichtuniversity.dke.agents.modules.vision;
 
 import nl.maastrichtuniversity.dke.agents.Direction;
 import nl.maastrichtuniversity.dke.discrete.Tile;
+import nl.maastrichtuniversity.dke.util.Position;
 
 import java.util.List;
 
 public interface IVisionModule {
-    List<Tile> getObstacles(Direction direction);
+    List<Tile> getObstacles(Position position,Direction direction);
 
 
 
