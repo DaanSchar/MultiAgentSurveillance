520710e46037643f6bf6bb9967cf90624e459630
Parand <p.mohri@student.maastrichtuniversity.nl>
2022-03-10 11:45:40 +0100

    fix agent list problem in memory

--- src/main/java/nl/maastrichtuniversity/dke/agents/modules/memory/MemoryModule.java
+++ src/main/java/nl/maastrichtuniversity/dke/agents/modules/memory/MemoryModule.java
@@ -12,6 +12,7 @@ import nl.maastrichtuniversity.dke.discrete.Scenario;
 import nl.maastrichtuniversity.dke.discrete.Tile;
 import nl.maastrichtuniversity.dke.util.Position;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -28,16 +29,28 @@ public class MemoryModule extends AgentModule implements IMemoryModule {
         int width = scenario.getEnvironment().getWidth();
         int height = scenario.getEnvironment().getHeight();
         this.map = new Environment(width, height, new Tile[width][height]);
+        agents = new ArrayList<>();
     }
 
+    //TODO: add sound to update method in memory
     public void update(IVisionModule vision) {
         for(Tile tile: vision.getObstacles()) {
             int x = tile.getPosition().getX();
             int y = tile.getPosition().getY();
             map.getTileMap()[x][y] = tile;
         }
-        agents = vision.getAgents();
+        for(Agent agentSee: vision.getAgents()){
+            if(agents.get(agentSee.getId()) != null){
+                agents.get(agentSee.getId()).setPosition(agentSee.getPosition());
+                agents.get(agentSee.getId()).setDirection(agentSee.getDirection());
+            }else{
+                agents.add(agentSee.getId(),agentSee.newInstance());
+            }
+
+        }
+
     }
+
 
 
 
