63eff21743f2becc08c0388adfdecfd844b9c95e
IgnacioCadarso <72268339+IgnacioCadarso@users.noreply.github.com>
2022-03-10 11:48:59 +0100

    fixed error

--- src/main/java/nl/maastrichtuniversity/dke/discrete/Sound.java
+++ src/main/java/nl/maastrichtuniversity/dke/discrete/Sound.java
@@ -5,9 +5,9 @@ import lombok.Setter;
 import nl.maastrichtuniversity.dke.util.Position;
 
 public class Sound {
-    private final @Getter @Setter Position source; //it holds the original tile where the sound is coming from
-    private final @Getter @Setter Position position; //in which tile this sound object  sound is
-    private final @Getter @Setter boolean isYell; //whether the sound is a yell that can be recognized by other guards
+    private  @Getter @Setter Position source; //it holds the original tile where the sound is coming from
+    private  @Getter @Setter Position position; //in which tile this sound object  sound is
+    private  @Getter @Setter boolean isYell; //whether the sound is a yell that can be recognized by other guards
 
     public Sound(Position position, Position source, boolean isYell){
         this.source = source;
