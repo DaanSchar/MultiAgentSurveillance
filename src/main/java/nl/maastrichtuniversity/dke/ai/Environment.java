aa33375cc6f348a84a6d0ae11df3185d77a50852
DaanSchar <Daan.Schaer@Gmail.com>
2022-03-19 18:55:02 +0100

    changes to training stuff

--- src/main/java/nl/maastrichtuniversity/dke/ai/Environment.java
+++ src/main/java/nl/maastrichtuniversity/dke/ai/Environment.java
@@ -1,25 +1,25 @@
 package nl.maastrichtuniversity.dke.ai;
 
+import lombok.extern.slf4j.Slf4j;
+import nl.maastrichtuniversity.dke.gui.GameWindow;
 import nl.maastrichtuniversity.dke.logic.Game;
 import nl.maastrichtuniversity.dke.logic.agents.Agent;
-import nl.maastrichtuniversity.dke.logic.scenario.environment.Tile;
-import nl.maastrichtuniversity.dke.logic.scenario.environment.TileType;
-import nl.maastrichtuniversity.dke.reinforcement.Reward;
 import org.deeplearning4j.gym.StepReply;
 import org.deeplearning4j.rl4j.mdp.MDP;
 import org.deeplearning4j.rl4j.space.DiscreteSpace;
 import org.deeplearning4j.rl4j.space.ObservationSpace;
 
-import java.util.LinkedList;
-
 /**
  * This is the environment that is used in the reinforcement learning.
  * It is kind of like a playground for the agents to play in.
  */
+@Slf4j
 public class Environment implements MDP<NeuralGameState, Integer, DiscreteSpace> {
 
     private final DiscreteSpace actionSpace = new DiscreteSpace(Network.NUM_OUTPUTS);
     private Game game;
+
+    private GameWindow window;
 
     @Override
     public StepReply<NeuralGameState> step(Integer integer) {
@@ -37,6 +37,8 @@ public class Environment implements MDP<NeuralGameState, Integer, DiscreteSpace>
             agent = agents.get(agentActions.size()-1);
         }
 
+        if (window != null) window.draw();
+
         return new StepReply<>(
                 new NeuralGameState(agent.getStateVector()),
                 Reward.calculateReward(game.getScenario()),
@@ -48,32 +50,20 @@ public class Environment implements MDP<NeuralGameState, Integer, DiscreteSpace>
     @Override
     public boolean isDone() {
         var scenario = game.getScenario();
-        int numberOfTiles = scenario.getEnvironment().getHeight() * scenario.getEnvironment().getWidth();
-        LinkedList<Tile> exploredTiles = new LinkedList<>();
+        int totalTiles = scenario.getEnvironment().getHeight() * scenario.getEnvironment().getWidth();
 
-        for (Agent a : scenario.getGuards()) {
-            for (Tile[] tCol : a.getMemoryModule().getMap().getTileMap()) {
-                for (Tile t : tCol) {
-                    if (t.getType()!= TileType.UNKNOWN && !exploredTiles.contains(t)) {
-                       exploredTiles.add(t);
-                    }
-                    if (exploredTiles.size() == numberOfTiles) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return exploredTiles.size() == numberOfTiles;
-
+        return scenario.getCoveredTiles().size() == totalTiles;
     }
 
     @Override
     public NeuralGameState reset() {
-        if (game == null) {
-            game = Game.getInstance();
-        } else {
-            game.reset();
-        }
+        if (game == null) { game = Game.getInstance(); }
+
+        double totalCoveredTiles = game.getScenario().getCoveredTiles().size();
+        double totalTiles = game.getScenario().getEnvironment().getHeight() * game.getScenario().getEnvironment().getWidth();
+
+        log.info("Total coverage: {}%", (totalCoveredTiles / totalTiles) * 100);
+        game.reset();
 
         return new NeuralGameState(game.getScenario().getGuards().get(0).getStateVector());
     }
