d303c4742375c15e585656b91931517f2fc8a65f
DaanSchar <Daan.Schaer@Gmail.com>
2022-03-19 18:17:39 +0100

    Made Environment iterable

--- src/main/java/nl/maastrichtuniversity/dke/logic/scenario/environment/Environment.java
+++ src/main/java/nl/maastrichtuniversity/dke/logic/scenario/environment/Environment.java
@@ -3,10 +3,11 @@ package nl.maastrichtuniversity.dke.logic.scenario.environment;
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 @Getter
-public class Environment {
+public class Environment implements Iterable<Tile> {
 
     private final int width;
     private final int height;
@@ -28,26 +29,79 @@ public class Environment {
     public List<Tile> get(TileType type) {
         List<Tile> tiles = new ArrayList<>();
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                if (tileMap[i][j].getType() == type) {
-                    tiles.add(tileMap[i][j]);
-                }
+        for (Tile tile : this) {
+            if (tile.getType() == type) {
+                tiles.add(tile);
             }
         }
+
         return tiles;
     }
 
-    public double[] getStateVector() {
-        double[] stateVector = new double[width * height];
 
-        for (int i = 0; i < width; i++) {
-            for (int j = 0; j < height; j++) {
-                stateVector[i * height + j] = tileMap[i][j].getType().getValue();
+    /**
+     * iterators may be used to iterate over the tiles in the environment
+     * you use them in a for-each loop like this:
+     *
+     *     for (Tile tile : environment) {
+     *         // do something with the tile
+     *     }
+     *
+     * @return an iterator over all tiles in the environment
+     */
+    @Override
+    public Iterator<Tile> iterator() {
+        return new EnvironmentIterator(this.tileMap);
+    }
+
+    public static class EnvironmentIterator implements Iterator<Tile> {
+
+        private final Tile[][] internalTileMap;
+        private final int width;
+        private final int height;
+        private int i, j;
+
+        public EnvironmentIterator(Tile[][] tileMap) {
+            this.internalTileMap = tileMap;
+            this.i = 0;
+            this.j = 0;
+            this.width = tileMap.length;
+            this.height = tileMap[0].length;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return !indexIsOutsideBounds();
+        }
+
+        @Override
+        public Tile next() {
+            Tile value = internalTileMap[i][j];
+            incrementIndex();
+
+            return value;
+        }
+
+        private boolean indexIsOutsideBounds() {
+            return indexIsTooSmall() || indexIsTooLarge();
+        }
+
+        private boolean indexIsTooSmall() {
+            return i < 0 || j < 0;
+        }
+
+        private boolean indexIsTooLarge() {
+            return i >= width|| j >= height;
+        }
+
+        private void incrementIndex() {
+            if (j >= height - 1) {
+                j = 0;
+                i++;
+            } else {
+                j++;
             }
         }
 
-        return stateVector;
     }
-
 }
