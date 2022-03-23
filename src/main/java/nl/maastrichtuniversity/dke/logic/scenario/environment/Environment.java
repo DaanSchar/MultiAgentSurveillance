8158b733dd882ef6e653545609f88673c79ba015
DaanSchar <Daan.Schaer@Gmail.com>
2022-03-23 22:24:54 +0100

    Added a filter method in Environment

--- src/main/java/nl/maastrichtuniversity/dke/logic/scenario/environment/Environment.java
+++ src/main/java/nl/maastrichtuniversity/dke/logic/scenario/environment/Environment.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 @Getter
@@ -43,6 +44,10 @@ public class Environment implements Collection<Tile> {
 
     public List<Tile> get(TileType type) {
         return this.stream().filter(tile -> tile.getType() == type).collect(Collectors.toList());
+    }
+
+    public List<Tile> filter(Predicate<Tile> predicate) {
+        return this.stream().filter(predicate).collect(Collectors.toList());
     }
 
     public List<Tile> getNeighbouringTiles(Tile tile) {
