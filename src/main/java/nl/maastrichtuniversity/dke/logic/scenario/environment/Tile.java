62aa85515c33154198d003a5f5a2f33a03ec9713
DaanSchar <Daan.Schaer@Gmail.com>
2022-03-27 20:43:13 +0200

    added isPassable() to Tile and MemoryTile

--- src/main/java/nl/maastrichtuniversity/dke/logic/scenario/environment/Tile.java
+++ src/main/java/nl/maastrichtuniversity/dke/logic/scenario/environment/Tile.java
@@ -4,31 +4,30 @@ import lombok.*;
 import nl.maastrichtuniversity.dke.logic.scenario.util.Position;
 
 @Getter
-//@AddArgsConstructor
 @EqualsAndHashCode
 @ToString
 public class Tile {
 
-    private @Getter Position position;
+    private @Getter final Position position;
     private boolean isOpened;
     private @Setter TileType type;
-//    private @Getter List<CommunicationMark> communicationMarks;
-
 
     public Tile(Position position) {
         this.position = position;
         this.type = TileType.EMPTY;
-        //this.communicationMarks = new ArrayList<>();
     }
 
     public Tile(Position position, TileType type) {
         this.position = position;
         this.type = type;
-       // this.communicationMarks = new ArrayList<>();
     }
 
     public boolean isEmpty(){
         return type == TileType.EMPTY;
+    }
+
+    public boolean isPassable() {
+        return type.isPassable();
     }
 
 
