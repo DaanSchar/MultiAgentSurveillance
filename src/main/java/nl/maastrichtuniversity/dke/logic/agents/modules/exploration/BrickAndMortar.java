115abce78db008bc363d34a5ff26866e0bec0cc9
DaanSchar <Daan.Schaer@Gmail.com>
2022-03-29 00:18:29 +0200

    removed unnecessary comments

--- src/main/java/nl/maastrichtuniversity/dke/logic/agents/modules/exploration/BrickAndMortar.java
+++ src/main/java/nl/maastrichtuniversity/dke/logic/agents/modules/exploration/BrickAndMortar.java
@@ -82,8 +82,8 @@ public class BrickAndMortar implements IExplorationModule {
     }
 
     /**
-     * @return true if the there exists no path between all explored/unexplored tiles would
-     *         the agent mark this tile as visited.
+     * true if the there exists no path between all
+     * explored/unexplored tiles would the agent mark this tile as visited.
      */
     private boolean currentCellBlocksPath() {
         List<Tile> neighbours = getPassableNeighbors(getCurrentTile());
@@ -97,9 +97,6 @@ public class BrickAndMortar implements IExplorationModule {
         return false;
     }
 
-    /**
-     * @return the move the agent should make to get to the best unexplored tile
-     */
     private MoveAction getMoveToBestUnexploredTile() {
         MemoryTile targetTile = getBestUnexploredTile();
         Position nextPosition = movementModule.goForward(currentPosition, currentDirection, Game.getInstance().getTime());
@@ -111,9 +108,6 @@ public class BrickAndMortar implements IExplorationModule {
         return MoveAction.ROTATE_LEFT;
     }
 
-    /**
-     * @return the move the agent should make to get to the best explored tile
-     */
     private MoveAction getMoveToBestExploredTile() {
         Tile targetTile = getBestExploredTile();
         Position nextPosition = movementModule.goForward(currentPosition, currentDirection, Game.getInstance().getTime());
@@ -123,9 +117,6 @@ public class BrickAndMortar implements IExplorationModule {
         return MoveAction.ROTATE_LEFT;
     }
 
-    /**
-     * @return the tile which the agent is currently standing on
-     */
     private MemoryTile getCurrentTile() {
         int x = currentPosition.getX();
         int y = currentPosition.getY();
@@ -133,14 +124,13 @@ public class BrickAndMortar implements IExplorationModule {
     }
 
     /**
-     * @return The MemoryTile adjacent to the agent which is most
-     * likely to be marked as visited in the marking step
-     *
      * the best tile is the tile which has the most adjacent non-passable tiles.
      */
     private MemoryTile getBestUnexploredTile() {
         List<Tile> unexploredTiles = getUnexploredNeighboringTiles(getCurrentTile());
 
+        // we shuffle the tiles, otherwise it will always return the tile from
+        // the same direction if all/most tiles are considered equally good
         if (Math.random() < randomness) { Collections.shuffle(unexploredTiles); }
 
         unexploredTiles.sort((t1 , t2) -> getNonPassableNeighbors(t2).size() - getNonPassableNeighbors(t1).size());
@@ -149,9 +139,8 @@ public class BrickAndMortar implements IExplorationModule {
     }
 
     /**
-     * @return The best tile considered as explored, where best
-     * means it will choose the first tile considered explored according to the
-     * direction priority.
+     * best tile means it will choose the first tile considered
+     * explored according to the direction priority.
      */
     private Tile getBestExploredTile() {
         for (Direction direction : directionPriority) {
@@ -163,9 +152,6 @@ public class BrickAndMortar implements IExplorationModule {
         return getCurrentTile();
     }
 
-    /**
-     * @return The Tile you would move to if you were to move forward
-     */
     private Tile getFacingTile(Tile tile, Direction direction) {
         int x = tile.getPosition().getX() + direction.getMoveX();
         int y = tile.getPosition().getY() + direction.getMoveY();
@@ -173,62 +159,34 @@ public class BrickAndMortar implements IExplorationModule {
         return environment.getTileMap()[x][y];
     }
 
-    /**
-     * @return true if the agent has any explored tiles adjacent to it
-     */
     private boolean hasNeighboringExploredTiles() {
         return getExploredNeighboringTiles(getCurrentTile()).size() > 0;
     }
 
-    /**
-     * @return true if the agent has any unexplored tiles adjacent to it
-     */
     private boolean hasNeighboringUnexploredTiles() {
         return getUnexploredNeighboringTiles(getCurrentTile()).size() > 0;
     }
 
-    /**
-     * @param tile The tile to check
-     * @return A list of tiles adjacent to the given tile which are non-passable
-     */
     private List<Tile> getNonPassableNeighbors(Tile tile) {
         return environment.getNeighborsAndFilter(tile, (neighbor) -> !neighbor.isPassable());
     }
 
-    /**
-     * @param tile The tile to check
-     * @return A list of tiles adjacent to the given tile which are passable
-     */
     private List<Tile> getPassableNeighbors(Tile tile) {
         return environment.getNeighborsAndFilter(tile, Tile::isPassable);
     }
 
-    /**
-     * @param tile The tile we get the neighboring tiles from
-     * @return A list of tiles which are adjacent to the agent and are not marked as explored
-     */
     private List<Tile> getUnexploredNeighboringTiles(Tile tile) {
         return environment.getNeighborsAndFilter(tile, (neighbor) -> !((MemoryTile)neighbor).isExplored() && neighbor.isPassable());
     }
 
-    /**
-     * @param tile The tile we get the neighboring tiles from
-     * @return A list of tiles which are adjacent to the agent and are marked as explored
-     */
     private List<Tile> getExploredNeighboringTiles(Tile tile) {
         return environment.getNeighborsAndFilter(tile, neighbor -> ((MemoryTile)neighbor).isExplored() && neighbor.isPassable());
     }
 
-    /**
-     * @return whether there is an existing path between two tiles.
-     */
     private boolean pathExists(Tile start, Tile end) {
-        return new BFS().search(start, end);
+        return new BFS().pathExists(start, end);
     }
 
-    /**
-     * Breadth-first search
-     */
     class BFS {
 
         private Queue<Tile> queue;
@@ -239,10 +197,7 @@ public class BrickAndMortar implements IExplorationModule {
             this.visited = new ArrayList<>();
         }
 
-        /**
-         * @return true if there exists a path between the two tiles
-         */
-        public boolean search(Tile start, Tile goal) {
+        public boolean pathExists(Tile start, Tile goal) {
             this.queue = new LinkedList<>();
             this.visited = new ArrayList<>();
             queue.add(start);
@@ -256,7 +211,8 @@ public class BrickAndMortar implements IExplorationModule {
 
                 for (Tile neighbour : getPassableNeighbors(current)) {
                     if (neighbour.equals(goal)) { return true; }
-                    // not including the tile which the agent is currently on
+                    // not including the tile which the agent is currently on, as we are trying to find a path
+                    // between 2 tiles, would the current tile be marked as visited.
                     if (!visited.contains(neighbour) && !neighbour.equals(getCurrentTile())) {
                         queue.add(neighbour);
                         visited.add(neighbour);
