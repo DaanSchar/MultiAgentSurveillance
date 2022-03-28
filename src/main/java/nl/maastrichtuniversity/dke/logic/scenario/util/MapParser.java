2b68e5b09497d55294be2d7824d2db15d6655c7c
DaanSchar <Daan.Schaer@Gmail.com>
2022-03-28 02:05:30 +0200

    removed annoying log statement

--- src/main/java/nl/maastrichtuniversity/dke/logic/scenario/util/MapParser.java
+++ src/main/java/nl/maastrichtuniversity/dke/logic/scenario/util/MapParser.java
@@ -127,8 +127,6 @@ public class MapParser {
                 (int)Double.parseDouble(values[6])
         );
 
-        log.info("Teleport added: {}, {}", values[4], values[5]);
-
         envBuilder.addTile(Integer.parseInt(values[4]), Integer.parseInt(values[5]), TileType.DESTINATION_TELEPORT);
     }
 
