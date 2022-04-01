d54247e0ab16e906a28c01ebccb68c4b83b44330
DaanSchar <Daan.Schaer@Gmail.com>
2022-04-01 21:13:26 +0200

    refactoring

--- core/src/com/mygdx/game/CameraController.java
+++ core/src/com/mygdx/game/CameraController.java
@@ -7,7 +7,7 @@ import com.badlogic.gdx.utils.viewport.FillViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import lombok.Getter;
 
-public class CameraController implements InputProcessor {
+public final class CameraController implements InputProcessor {
 
     private @Getter Viewport viewport;
     private @Getter OrthographicCamera camera;
@@ -15,42 +15,48 @@ public class CameraController implements InputProcessor {
     private final float worldWidth;
     private final float worldHeight;
 
-    private final float MAX_ZOOM = 2.0f;
-    private final float MIN_ZOOM = 0.1f;
-    private final float ZOOM_STEP = 0.05f;
     private final float DRAG_MULTIPLIER;
 
     public CameraController(EnvironmentView environmentView) {
         this.worldWidth = environmentView.getWidth();
         this.worldHeight = environmentView.getHeight();
         this.DRAG_MULTIPLIER = (Gdx.graphics.getWidth() / worldWidth) * 20f;
+
         setupViewPort();
     }
 
     private void setupViewPort() {
-        float aspectRatio = (float) Gdx.graphics.getHeight() / (float) Gdx.graphics.getWidth();
         camera = new OrthographicCamera();
-        camera.position.set(worldWidth / 4f, worldHeight / 4f, 0);
-        viewport = new FillViewport(worldWidth * aspectRatio, worldHeight * aspectRatio, camera);
+        centerCameraPosition();
+
+        viewport = new FillViewport(worldWidth, worldHeight, camera);
         viewport.apply();
     }
 
     @Override
     public boolean scrolled(float amountX, float amountY) {
-        if (amountY > 0 && camera.zoom < MAX_ZOOM)
-            camera.zoom += ZOOM_STEP;
-        else if (amountY < 0 && camera.zoom > MIN_ZOOM)
-            camera.zoom -= ZOOM_STEP;
+        float maxZoom = 2.0f;
+        float minZoom = 0.1f;
+        float zoomStep = 0.05f;
+
+        if (amountY > 0 && camera.zoom < maxZoom) {
+            camera.zoom += zoomStep;
+        } else if (amountY < 0 && camera.zoom > minZoom) {
+            camera.zoom -= zoomStep;
+        }
 
         return true;
     }
 
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
-        float x = Gdx.input.getDeltaX();
-        float y = Gdx.input.getDeltaY();
+        float dragX = Gdx.input.getDeltaX();
+        float dragY = Gdx.input.getDeltaY();
 
-        camera.translate(-x * DRAG_MULTIPLIER * camera.zoom, y * DRAG_MULTIPLIER * camera.zoom);
+        float moveCameraX = -dragX * DRAG_MULTIPLIER * camera.zoom;
+        float moveCameraY = dragY * DRAG_MULTIPLIER * camera.zoom;
+
+        camera.translate(moveCameraX, moveCameraY);
         return true;
     }
 
@@ -82,5 +88,11 @@ public class CameraController implements InputProcessor {
     @Override
     public boolean mouseMoved(int screenX, int screenY) {
         return false;
+    }
+
+    private void centerCameraPosition() {
+        float middleX = worldWidth / 2f;
+        float middleY = worldHeight / 2f;
+        camera.position.set(middleX, middleY, 0);
     }
 }
