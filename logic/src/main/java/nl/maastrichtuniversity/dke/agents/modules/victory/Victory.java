e5a35831601c0749045d0beac40090accd8abf4c
IgnacioCadarso <72268339+IgnacioCadarso@users.noreply.github.com>
2022-06-08 14:01:02 +0200

    added gamemode 0

--- logic/src/main/java/nl/maastrichtuniversity/dke/agents/modules/victory/Victory.java
+++ logic/src/main/java/nl/maastrichtuniversity/dke/agents/modules/victory/Victory.java
@@ -2,6 +2,7 @@ package nl.maastrichtuniversity.dke.agents.modules.victory;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import nl.maastrichtuniversity.dke.agents.Intruder;
 import nl.maastrichtuniversity.dke.scenario.Scenario;
 import nl.maastrichtuniversity.dke.scenario.environment.Tile;
@@ -9,33 +10,60 @@ import nl.maastrichtuniversity.dke.scenario.environment.TileType;
 
 import java.util.List;
 
+@Slf4j
 @Setter
 @Getter
 public class Victory implements IVictory {
     private Scenario scenario;
     private String winner = "";
+    private final int ogNumberOfIntruders;
 
     public Victory(Scenario scenario) {
         this.scenario = scenario;
+        this.ogNumberOfIntruders = scenario.getIntruders().size();
     }
 
     public boolean intrudersHaveWon() {
-        for (Intruder intruder : scenario.getIntruders()) {
-            if (intruderIsOnTargetTile(intruder)) {
-                return true;
+        //GAME MODE 1
+        if (scenario.getGameMode() == 1) {
+            for (Intruder intruder : scenario.getIntruders()) {
+                if (intruderIsOnTargetTile(intruder)) {
+                    return true;
+                }
             }
-        }
 
-        return false;
+            return false;
+        }
+        //GAME MODE 0
+        else if (scenario.getGameMode() == 0) {
+            int count = 0;
+            for (Intruder intruder : scenario.getIntruders()) {
+                if (intruderIsOnTargetTile(intruder)) {
+                    count += 1;
+                }
+            }
+            if (count == scenario.getIntruders().size()) {
+                return true;
+            } else return false;
+        } else return false;
     }
 
     public boolean guardsHaveWon() {
-        for (Intruder intruder : scenario.getIntruders()) {
-            if (!intruder.isCaught()) {
-                return false;
+        // GAME MODE 1
+        if (scenario.getGameMode() == 1) {
+            for (Intruder intruder : scenario.getIntruders()) {
+                if (!intruder.isCaught()) {
+                    return false;
+                }
             }
+            return true;
         }
-        return true;
+        // GAME MODE 0
+        else if (scenario.getGameMode() == 0) {
+            if (scenario.getIntruders().size() < ogNumberOfIntruders) {
+                return true;
+            } else return false;
+        } else return false;
     }
 
     private boolean intruderIsOnTargetTile(Intruder intruder) {
